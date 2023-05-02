a820507d0d3862d6ab8a8ceda787e5a6d874f556
Eya Gargah <eyagargah@hotmail.com>
2023-05-02 13:12:54 +0100

    Update app.component.ts
    theme change background color and add transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'todo-app';
   src = 'assets/images/bg-desktop-light.jpg';
-
   light: boolean | undefined;
 
+  body = document.querySelector('body')
   ngOnInit() {}
 
   newthemeChange(light: any) {
     this.light = light;
     if (this.light) {
       this.src = 'assets/images/bg-desktop-light.jpg';
+      if(this.body){
+        this.body.style.backgroundColor = "hsl(236, 33%, 92%)"
+        this.body.style.transition = "1s ease "
+
+
+      }
     } else {
       this.src = 'assets/images/bg-desktop-dark.jpg';
+      if(this.body){
+        this.body.style.backgroundColor = "hsl(235, 21%, 11%)"
+        this.body.style.transition = "1s ease "
+      }
     }
   }
 }
