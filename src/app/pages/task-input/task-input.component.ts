8eb8ae987579a1d2f1003f17bc37bb9df1675ecf
Eya Gargah <eyagargah@hotmail.com>
2023-05-10 20:30:25 +0100

    Update task-input.component.ts

--- src/app/pages/task-input/task-input.component.ts
+++ src/app/pages/task-input/task-input.component.ts
@@ -5,47 +5,51 @@ import { DataService } from 'src/app/data.service';
 @Component({
   selector: 'app-task-input',
   templateUrl: './task-input.component.html',
-  styleUrls: ['./task-input.component.scss']
+  styleUrls: ['./task-input.component.scss'],
 })
 export class TaskInputComponent {
+  input = document.querySelector('input');
+  light: any;
+  task: any;
+  completed : any
+  constructor(private data: DataService) {}
 
-  input = document.querySelector('input')
-  light : any 
-  task : any
-  constructor(private data: DataService){
+  ngOnInit() {
+    this.data.theme.subscribe((light) => {
+      return (this.light = light);
+    });
+    this.changeTheme();
   }
 
-  ngOnInit(){
-    this.data.theme.subscribe( light => {
-      console.log("🚀 ~ file: task-input.component.ts:18 ~ TaskInputComponent ~ ngOnInit ~ light:", light)
-      return this.light = light
-    })
-    this.changeTheme()
-  }
+  addTask = async (e: any) => {
+    if (e.key == 'Enter') {
+      try {
+        this.task = e.target.value;
+        this.completed = false
+        const response = await axios.post('http://localhost:8000/', {
+          task: this.task,
+          completed: this.completed
+        });
+        const success = response.status === 201;
 
- addTask = async(e: any)=>  {
-  if(e.key == "Enter"){
-    const response = await axios.get('http://localhost:8000/new', { task : e.target.value})
-    
-    console.log(e.target.value)
-    const success = response.status === 201;
-  }
- }
-    
-changeTheme(){
-  if(this.light){
-    if(this.input){
-      this.input.style.backgroundColor = 'white';
+        if (success) {
+          console.log('success');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  changeTheme() {
+    if (this.light) {
+      if (this.input) {
+        this.input.style.backgroundColor = 'white';
+      }
+    } else {
+      if (this.input) {
+        this.input.style.backgroundColor = 'hsl(235, 24%, 19%)';
+      }
     }
   }
-  else {
-    if(this.input){
-      this.input.style.backgroundColor = 'hsl(235, 24%, 19%)';
-    }
-  }
-}
-   
-    
-
-
 }
