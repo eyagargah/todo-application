02cee7aaf0f072583bf3b3552ac2955c18c2acba
Eya Gargah <eyagargah@hotmail.com>
2023-05-10 20:30:22 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const uri = 'mongodb+srv://gargaheya:mypassword@cluster0.nfdu7oj.mongodb.net/'
 
 const app = express()
 
-
+app.use(express.json())
 /*******Cors********/
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
@@ -34,24 +34,27 @@ app.get('/tasks', async(req , res)=> {
         const tasks = db.collection('task')
 
         const returnedTasks = await tasks.find().toArray()
+
         res.send(returnedTasks)
     } finally{
         await client.close()
     }
 })
 
-app.post('/new', async(req , res)=> {
+app.post('/', async(req , res)=> {
     const client = new MongoClient(uri)
-    const { task } = req.body
+    const { task , completed } = req.body
+    
     try {
         await client.connect()
         const db = client.db('app-data')
         const tasks = db.collection('task')
 
         const data = {
-            task : task
+            task : task,
+            completed: completed
         }
-        res.send(returnedTasks)
+        await tasks.insertOne(data)
     } finally{
         await client.close()
     }
