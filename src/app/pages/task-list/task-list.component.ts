f88b6e6a1fa91c6b4fc25fe6b7c8c883f28d6763
Eya Gargah <eyagargah@hotmail.com>
2023-05-10 22:13:48 +0100

    Update task-list.component.ts
    add check task and line through task content when checked

--- src/app/pages/task-list/task-list.component.ts
+++ src/app/pages/task-list/task-list.component.ts
@@ -10,10 +10,14 @@ import { DataService } from 'src/app/data.service';
 export class TaskListComponent {
   light: any;
   tasks: any
+
+  isChecked = false
   constructor(private data: DataService){}
 ngOnInit(){
   this.data.theme.subscribe( light => this.light = light)
   this.getTasks()
+  
+
 
 }
 
@@ -23,6 +27,34 @@ getTasks = async()=> {
   const success = response.status ===201
 
   this.tasks = response.data
+}
+
+checkTask(){
+  const checkBtn = document.querySelector('.checkBtn')
+  const deleteBtn = document.querySelector('.deleteBtn')
+  const tickImg = document.querySelector('.checkedImg')
+  var  task = document.querySelector('.taskContent') as HTMLElement
+  
+  if(!this.isChecked && task){
+    this.isChecked = true
+    tickImg?.classList.remove('hide')
+    tickImg?.classList.add('show')
+    checkBtn?.classList.add('checked')
+    task.style.textDecoration = "line-through"
+
+  }else
+  if(this.isChecked && task){
+    this.isChecked = false
+    tickImg?.classList.remove('show')
+    tickImg?.classList.add('hide')
+    checkBtn?.classList.remove('checked')
+    task.style.textDecoration = "none"
+  }
+  
+}
+
+deleteTask(){
+  console.log("delete")
 }
 
 }
