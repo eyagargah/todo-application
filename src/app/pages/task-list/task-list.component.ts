664f8b32e8f2dca51c306c7515594e92bb4853b9
Eya Gargah <eyagargah@hotmail.com>
2023-05-13 22:43:52 +0100

    Update task-list.component.ts

--- src/app/pages/task-list/task-list.component.ts
+++ src/app/pages/task-list/task-list.component.ts
@@ -5,56 +5,51 @@ import { DataService } from 'src/app/data.service';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent {
   light: any;
-  tasks: any
+  tasks: any;
 
-  isChecked = false
-  constructor(private data: DataService){}
-ngOnInit(){
-  this.data.theme.subscribe( light => this.light = light)
-  this.getTasks()
-  
-
-
-}
-
-getTasks = async()=> {
-  const response = await axios.get('http://localhost:8000/tasks')
-
-  const success = response.status ===201
-
-  this.tasks = response.data
-}
-
-checkTask(){
-  const checkBtn = document.querySelector('.checkBtn')
-  const deleteBtn = document.querySelector('.deleteBtn')
-  const tickImg = document.querySelector('.checkedImg')
-  var  task = document.querySelector('.taskContent') as HTMLElement
-  
-  if(!this.isChecked && task){
-    this.isChecked = true
-    tickImg?.classList.remove('hide')
-    tickImg?.classList.add('show')
-    checkBtn?.classList.add('checked')
-    task.style.textDecoration = "line-through"
-
-  }else
-  if(this.isChecked && task){
-    this.isChecked = false
-    tickImg?.classList.remove('show')
-    tickImg?.classList.add('hide')
-    checkBtn?.classList.remove('checked')
-    task.style.textDecoration = "none"
+  isChecked = false;
+  constructor(private data: DataService) {}
+  ngOnInit() {
+    this.data.theme.subscribe((light) => (this.light = light));
+    this.getTasks();
   }
-  
-}
 
-deleteTask(){
-  console.log("delete")
-}
+  getTasks = async () => {
+    const response = await axios.get('http://localhost:8000/tasks');
 
+    this.tasks = response.data;
+  };
+
+  checkTask(e: any) {
+    let checkBtn = e.target.parentElement;
+    let checkImg = e.target;
+    
+    let taskContent = e.target.parentElement.parentElement.childNodes[1]
+    let text = e.target.parentElement.parentElement.childNodes[1].innerHTML
+    console.log(taskContent)
+    if (!this.isChecked) {
+      this.isChecked = true;
+      checkBtn?.classList.add('isChecked');
+      checkImg?.classList.remove('hide');
+      taskContent.classList.add('strike')
+ 
+    } else if(this.isChecked){
+      this.isChecked = false;
+      checkBtn?.classList.remove('isChecked');
+      checkImg?.classList.add('hide');
+      taskContent.classList.remove('strike')
+      
+      
+      
+    }
+  }
+
+  deleteTask(e: any) {
+    let el = (e.target as HTMLElement).parentElement?.parentElement;
+    console.log(el);
+  }
 }
