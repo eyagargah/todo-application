65e17ef9bfa23d680114568251c2786cb8c3bdd6
Eya Gargah <eyagargah@hotmail.com>
2023-05-14 18:44:44 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/header/header.component';
 import { TaskComponent } from './pages/task/task.component';
 import { TaskListComponent } from './pages/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     TaskComponent,
     TaskListComponent
   ],
