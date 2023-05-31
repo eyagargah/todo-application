243264785392d2be82c6554b6218b8c50a333b23
Eya Gargah <eyagargah@hotmail.com>
2023-05-31 21:26:49 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,22 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient , HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './pages/task/task.component';
 import { TaskListComponent } from './pages/task-list/task-list.component';
 
-let tasks:any = basicLoader
+let tasks: any;
 
-export function basicLoader(httpClient: HttpClient){
-  return ()=> {
-    const response =  httpClient.get('http://localhost:8000/tasks');
-    
-    return response.subscribe( r=> console.log(r))
-
-  }
-}
 @NgModule({
   declarations: [AppComponent, TaskComponent, TaskListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: basicLoader,
-      deps: [HttpClient],
-      multi: true,
-    },
-    AppModule
-  ],
+  providers: [AppModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {
   static light: any;
   static tasks: any;
-  
-  ngOnInit(){
-    
-  }
+
+  ngOnInit() {}
 }
