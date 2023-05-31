4d34d0af8ecf2c6645d707160aae25d8a7c46fae
Eya Gargah <eyagargah@hotmail.com>
2023-05-31 21:20:43 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient , HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './pages/task/task.component';
 import { TaskListComponent } from './pages/task-list/task-list.component';
 
+let tasks:any = basicLoader
+
+export function basicLoader(httpClient: HttpClient){
+  return ()=> {
+    const response =  httpClient.get('http://localhost:8000/tasks');
+    
+    return response.subscribe( r=> console.log(r))
+
+  }
+}
 @NgModule({
-  declarations: [
-    AppComponent,
-    TaskComponent,
-    TaskListComponent
+  declarations: [AppComponent, TaskComponent, TaskListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: basicLoader,
+      deps: [HttpClient],
+      multi: true,
+    },
+    AppModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  static light: any;
+  static tasks: any;
+  
+  ngOnInit(){
+    
+  }
+}
