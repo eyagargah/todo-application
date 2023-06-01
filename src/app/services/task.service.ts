9f34ec14a5b182442adf18c75d985c486cf38000
Eya Gargah <eyagargah@hotmail.com>
2023-06-01 21:56:27 +0100

    Create task.service.ts

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTasks(){
+    return JSON.parse(localStorage.getItem('tasks')|| '[]')
+    
+  }
+
+  setTasks(tasks: any){
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+  }
+}
