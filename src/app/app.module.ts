23a68945972f735967caad2aa1e68f599f5d5432
Eya Gargah <eyagargah@hotmail.com>
2023-06-01 21:57:19 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
 import { TaskComponent } from './pages/task/task.component';
 import { TaskListComponent } from './pages/task-list/task-list.component';
+import { TaskInputComponent } from './pages/task-input/task-input.component';
 
-let tasks: any;
 
 @NgModule({
-  declarations: [AppComponent, TaskComponent, TaskListComponent],
+  declarations: [AppComponent, TaskComponent, TaskListComponent, HeaderComponent, TaskInputComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [AppModule],
   bootstrap: [AppComponent],
